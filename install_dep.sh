26850117d185e3e1c9aba092bcd5db76853bc1f5
Matvey <2663161@mail.ru>
2020-04-25 11:07:49 +0300

    commit

--- install_dep.sh
+++ install_dep.sh
@@ -7,7 +7,14 @@ sudo apt-get -y install ros-melodic-gazebo-ros \
                      ros-melodic-geographic-msgs \
                      python-jinja2
 
-sudo apt-get update && sudo apt-get -y --quiet --no-install-recommends install python-pip python3-pip bzip2 ca-certificates ccache cmake cppcheck curl dirmngr doxygen file g++ gcc gdb git gnupg gosu lcov libfreetype6-dev libgtest-dev libpng-dev lsb-release make ninja-build openjdk-8-jdk openjdk-8-jre openssh-client pkg-config python-pip python-pygments python-setuptools rsync shellcheck tzdata unzip wget xsltproc zip ant gazebo7 gstreamer1.0-plugins-bad gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly libeigen3-dev libgazebo7-dev libgstreamer-plugins-base1.0-dev libimage-exiftool-perl libopencv-dev libxml2-utils pkg-config protobuf-compiler libgeographic-dev geographiclib-tools libignition-math2-dev
-sudo pip install --user setuptools pkgconfig wheel && pip install --user docopt argparse argcomplete coverage jinja2 empy numpy requests serial toml pyyaml cerberus
-sudo pip3 install --user setuptools pkgconfig wheel && pip install --user docopt argparse argcomplete coverage jinja2 empy numpy requests serial toml pyyaml cerberus
+sudo apt-get update
+
+sudo apt-get -y --quiet --no-install-recommends install python-pip python3-pip bzip2 ca-certificates ccache cmake cppcheck curl dirmngr doxygen file g++ gcc gdb git gnupg gosu lcov libfreetype6-dev libgtest-dev libpng-dev lsb-release make ninja-build openjdk-8-jdk openjdk-8-jre openssh-client pkg-config python-pip python-pygments python-setuptools rsync shellcheck tzdata unzip wget xsltproc zip ant gazebo9 gstreamer1.0-plugins-bad gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly libeigen3-dev libgazebo9-dev libgstreamer-plugins-base1.0-dev libimage-exiftool-perl libopencv-dev libxml2-utils pkg-config protobuf-compiler libgeographic-dev geographiclib-doc libignition-math2-dev
+
+sudo pip install setuptools pkgconfig wheel
+sudo pip install docopt argparse argcomplete coverage jinja2 empy numpy requests serial toml pyyaml cerberus
+
+sudo pip3 install setuptools pkgconfig wheel
+sudo pip install docopt argparse argcomplete coverage jinja2 empy numpy requests serial toml pyyaml cerberus
+
 wget -qO- https://raw.githubusercontent.com/mavlink/mavros/master/mavros/scripts/install_geographiclib_datasets.sh | sudo bash
