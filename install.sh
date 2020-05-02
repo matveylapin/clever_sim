b6643f1f113c053089f6bf58b45cf8abb247038a
Matvey <2663161@mail.ru>
2020-05-02 21:39:09 +0300

    сщььше

--- install.sh
+++ install.sh
@@ -19,10 +19,10 @@ echo -e "\e[1;42m ROS installation is successful \e[0m"
 
 sudo apt -y install python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential python-catkin-tools
 {
-    sudo rosdep init
+    rosdep init
 } || {
     {
-        rosdep init
+        sudo rosdep init
     } || {
         sudo rm -rf /etc/ros/rosdep/sources.list.d/20-default.list
         {
@@ -52,6 +52,7 @@ sudo apt -y install ros-melodic-gazebo-ros \
 
 
 sudo apt-get -y install python-pip python3-pip catkin
+sudo apt -y install catkin
 sudo apt-get -y --quiet --no-install-recommends install bzip2 ca-certificates ccache cmake cppcheck curl dirmngr doxygen file g++ gcc gdb git gnupg gosu lcov libfreetype6-dev libgtest-dev libpng-dev lsb-release make ninja-build openjdk-8-jdk openjdk-8-jre openssh-client pkg-config python-pip python-pygments python-setuptools rsync shellcheck tzdata unzip wget xsltproc zip ant gazebo9 gstreamer1.0-plugins-bad gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly libeigen3-dev libgazebo9-dev libgstreamer-plugins-base1.0-dev libimage-exiftool-perl libopencv-dev libxml2-utils pkg-config protobuf-compiler libgeographic-dev libignition-math2-dev
 
 sudo apt install -y geographiclib-tools
@@ -63,6 +64,7 @@ sudo pip3 install setuptools pkgconfig wheel catkin_tools
 sudo pip3 install docopt argparse argcomplete coverage jinja2 empy numpy requests serial toml pyyaml cerberus
 
 wget -qO- https://raw.githubusercontent.com/mavlink/mavros/master/mavros/scripts/install_geographiclib_datasets.sh | sudo bash
+source /opt/ros/melodic/setup.bash
 echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
 ### CLEVER
