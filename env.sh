9c5d5bf47c989be30a920e36762e14a6f77c5b58
Matvey <2663161@mail.ru>
2020-05-02 18:34:38 +0300

    commit

--- env.sh
+++ env.sh
@@ -2,5 +2,6 @@ source catkin_ws/devel/setup.bash
 source Firmware/Tools/setup_gazebo.bash $(pwd) $(pwd)/Firmware/build/px4_sitl_default
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)/Firmware
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)/Firmware/Tools/sitl_gazebo
-export GAZEBO_MODEL_PATH=$(pwd)/models:$GAZEBO_MODEL_PATH
-#export GAZEBO_MODEL_PATH=$(pwd)/Firmware/Tools/sitl_gazebo/models:$GAZEBO_MODEL_PATH
+export GAZEBO_RESOURCE_PATH=$(pwd)/Firmware/Tools/sitl_gazebo/worlds:$GAZEBO_RESOURCE_PATH
+export GAZEBO_MODEL_PATH=$(pwd)/Firmware/Tools/sitl_gazebo/models:$GAZEBO_MODEL_PATH
+export SVGA_VGPU10=0
