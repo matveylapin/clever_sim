4200de93b0b09a9e7779af94d634a336076d09f9
Matvey <2663161@mail.ru>
2020-05-02 17:08:04 +0300

    commit

--- patch_clever.sh
+++ patch_clever.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 # Clever for PC
+{
 sudo patch < ../../../../clever_sim/patches/CMakeLists.patch
 
 sudo cp -f ../../../../clever_sim/patches/files_for_clover/launch/aruco.launch ./launch/
 sudo cp -f ../../../../clever_sim/patches/files_for_clover/launch/sitl.launch ./launch/
 sudo cp -f ../../../../clever_sim/patches/files_for_clover/map.txt ../aruco_pose/map/
+} || {
+    echo -e "\e[1;31m patch_clever.sh error \e[0m"
+    exit
+}
+
+echo -e "\e[1;42m patch clever is succescful \e[0m"
